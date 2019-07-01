c0752dfe72060aa6cd23ddf435a9529bcd440359
jasperOosterbroek <jasper.oosterbroek@student.hu.nl>
2019-07-01 17:46:33 +0200

    Made small demo code for r2d2 demos

--- code/headers/temperature_module.hpp
+++ code/headers/temperature_module.hpp
@@ -2,6 +2,7 @@
 
 #include <base_module.hpp>
 #include <mlx90615.hpp>
+#include <cstring>
 
 namespace r2d2::temperature_sensor {
     class module_c : public base_module_c {
@@ -15,19 +16,74 @@ namespace r2d2::temperature_sensor {
         }
 
         void process() override {
+            r2d2::frame_display_8x8_character_s display_temperature_frame_ambient;
+            display_temperature_frame_ambient.x = 64;
+            display_temperature_frame_ambient.y = 0;
+            display_temperature_frame_ambient.red = 255;
+            display_temperature_frame_ambient.green = 0;
+            display_temperature_frame_ambient.blue = 0;
+
+            uint16_t a_temp = static_cast<int16_t>(mlx.get_ambient_temperature());
+            char a_temp_arr[243] = {0};
+
+            a_temp_arr[5] = (a_temp % 10) + 48;
+            a_temp /= 10;
+            a_temp_arr[4] = (a_temp % 10) + 48;
+            a_temp /= 10;
+            a_temp_arr[3] = ',';
+            a_temp_arr[2] = (a_temp % 10) + 48;
+            a_temp /= 10;
+            a_temp_arr[1] = (a_temp % 10) + 48;
+            a_temp /= 10;
+            a_temp_arr[0] = (a_temp % 10) + 48;
+
+            strcpy(display_temperature_frame_ambient.characters, a_temp_arr); 
+            comm.send(display_temperature_frame_ambient);
+
+            r2d2::frame_display_8x8_character_s display_temperature_frame_object;
+            display_temperature_frame_object.x = 64;
+            display_temperature_frame_object.y = 8;
+            display_temperature_frame_object.red = 255;
+            display_temperature_frame_object.green = 0;
+            display_temperature_frame_object.blue = 0;
+
+            uint16_t o_temp = static_cast<int16_t>(mlx.get_object_temperature());
+            char o_temp_arr[243] = {0};
+
+            o_temp_arr[5] = (o_temp % 10) + 48;
+            o_temp /= 10;
+            o_temp_arr[4] = (o_temp % 10) + 48;
+            o_temp /= 10;
+            o_temp_arr[3] = ',';
+            o_temp_arr[2] = (o_temp % 10) + 48;
+            o_temp /= 10;
+            o_temp_arr[1] = (o_temp % 10) + 48;
+            o_temp /= 10;
+            o_temp_arr[0] = (o_temp % 10) + 48;
+
+            strcpy(display_temperature_frame_object.characters, o_temp_arr); 
+            comm.send(display_temperature_frame_object);
+
+            /*
             bool has_send = false;
             while (comm.has_data()) {
                 auto frame = comm.get_data();
                 if (!frame.request && !has_send) {
                     continue;
                 }
+
                 frame_temperature_s temperatures{
                     mlx.get_id(),
                     static_cast<int16_t>(mlx.get_ambient_temperature()),
                     static_cast<int16_t>(mlx.get_object_temperature())};
-                comm.send(temperatures);
+                comm.send(temperatures); 
                 has_send = true;
+
             }
+            */
+
         }
+
+         
     };
 } // namespace r2d2::temperature_sensor
--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,5 @@
 #include <comm.hpp>
+#include <cstring>
 #include <temperature_module.hpp>
 
 int main() {
@@ -10,8 +11,31 @@ int main() {
                                  50'000);
     r2d2::temperature_sensor::mlx90615 mlx90615(i2c_bus);
     r2d2::temperature_sensor::module_c module(comm, mlx90615);
+    r2d2::frame_display_8x8_character_s display_temperature_frame_amb;
+    r2d2::frame_display_8x8_character_s display_temperature_frame_obj;
+    display_temperature_frame_amb.x = 0;
+    display_temperature_frame_amb.y = 0;
+    display_temperature_frame_amb.red = 255;
+    display_temperature_frame_amb.green = 0;
+    display_temperature_frame_amb.blue = 0;
+    char temprature_a[243] = {'A', '_', 't', 'e', 'm', 'p', ':', ' '}; // 8 char : x = 64
 
+    display_temperature_frame_obj.x = 0;
+    display_temperature_frame_obj.y = 8;
+    display_temperature_frame_obj.red = 255;
+    display_temperature_frame_obj.green = 0;
+    display_temperature_frame_obj.blue = 0;
+    char temprature_o[243] = {'O', '_', 't', 'e', 'm', 'p', ':', ' '}; // 8 char : x = 64
+
+    strcpy(display_temperature_frame_amb.characters, temprature_a);
+    comm.send(display_temperature_frame_amb);
+    
+    strcpy(display_temperature_frame_obj.characters, temprature_o);
+    comm.send(display_temperature_frame_obj);
     while (true) {
         module.process();
+        hwlib::wait_ms(1000);
+
     }
+
 }
